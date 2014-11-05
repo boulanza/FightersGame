acab127ffc97b710606589e491f4b11ad502da39
boulanza <boulanza@umich.edu>
2014-11-05 12:40:22 -0500

    added form functionality to create user, updated game "screen" to display some info

--- Script.js
+++ Script.js
@@ -5,6 +5,10 @@
 var allTypes = ["Fighter", "Spearman", "Archer", "Knight", "Assassin"];
 var allSkills = ["StrUp", "DefUp", "SpdUp", "HpUp", "Allergy", "Cloak", "Rock", "Look", "Snack"];
 var allPlayers = [];
+var user;
+var challenger = 0;
+var message = "";
+var distance;
 
 function Player(hp, str, def, spd, type, skill) {
     this.hp = hp;
@@ -36,16 +40,7 @@ function genPlayers(numPlayers) {
 //                                           Running                                             //
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-genPlayers(9);
-var out = "<ul>";
 
-for (var i = 0; i < 9; ++i) {
-    out += "<li>";
-    out += "hp: " + allPlayers[i].hp + " str: " + allPlayers[i].str;
-    out += " def: " + allPlayers[i].def + " spd: " + allPlayers[i].spd;
-    out += " type: " + allPlayers[i].type + " skill: " + allPlayers[i].skill;
-    out += "</li>";
-}
 
 function drownDownFill() {
     var select = document.getElementById('usrType');
@@ -68,7 +63,6 @@ function drownDownFill() {
 }
 
 window.onload = function () {
-    $('#displayBox').html(out);
     drownDownFill();
 }
 
@@ -79,4 +73,60 @@ function displayModal() {
 
 function createGame() {
     displayModal();
+
+    genPlayers(9);
+
+    var hp  = document.getElementById("usrHp").value;
+    var str = document.getElementById("usrStr").value;
+    var def = document.getElementById("usrDef").value;
+    var spd = document.getElementById("usrSpd").value;
+    var type = document.getElementById("usrType").value;
+    var skill = document.getElementById("usrSkill").value;
+    user = new Player(hp, str, def, spd, type, skill);
+
+    message = "<ul>";
+    message += "<li>Welcome to the Pillow Arena of DOOOOOOOM! Challenger are you ready to rumble?</li>";
+    $('#displayBox').html(message);
+
+    var inp = "<a href='#' onclick='gameTurn()'>ready?</a>";
+    $('#inputBox').html(inp);
+}
+
+function printStatus() {
+    var status = "<table><tr>"
+    status += "<td><b>You:</b><br />";
+    status += "health: " + user.hp + "<br />";
+    status += "strength: " + user.str + "<br />";
+    status += "defense: " + user.def + "<br />"
+    status += "speed: " + user.spd + "<br />";
+    status += "Class: " + user.type + "<br />";
+    status += "Skill: " + user.skill + "<br /></td>";
+
+    status += "<td><b>Opponent:</b><br />";
+    status += "health: " + allPlayers[challenger].hp + "<br />";
+    status += "strength: " + allPlayers[challenger].str + "<br />";
+    status += "defense: " + allPlayers[challenger].def + "<br />"
+    status += "speed: " + allPlayers[challenger].spd + "<br />";
+    status += "Class: " + allPlayers[challenger].type + "<br />";
+    status += "Skill: " + allPlayers[challenger].skill + "<br /></td></tr>";
+
+    status += "<tr><td>Distance between you two: " + distance + "<br /></td></tr>";
+
+    status += "<tr><td>insert move things here</td></tr>";
+
+    status += "</table>";
+    $('#inputBox').html(status);
+}
+
+function gameTurn() {
+    distance = 5;
+
+    message += "<li>You are currently facing Opponent #" + challenger + "!</li>";
+    message += "<li>3...</li>";
+    message += "<li>2...</li>";
+    message += "<li>1...</li>";
+    message += "<li>GO!</li>";
+
+    $('#displayBox').html(message);
+    printStatus();
 }
